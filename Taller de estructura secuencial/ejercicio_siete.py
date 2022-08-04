57f0e11b3270095488f2474a2f83327ac3ed662c
Tomás Puchana <tpuchan33373@universidadean.edu.co>
2022-08-04 17:58:52 -0500

    ejercicio_siete

---
+++ Taller de estructura secuencial/ejercicio_siete.py
@@ -0,0 +1,6 @@
+valor=float(input("Escriba un valor en metros: "))
+
+pulgadas=(valor*39.37)
+pies=(pulgadas)/12
+
+print(f"El valor de la conversión a pulgadas es de: {pulgadas} pulgadas , El valor de la conversión a pies es de: {pies} pies")
