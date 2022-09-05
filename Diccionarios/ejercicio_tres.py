99e263e8170891851d71e01d4364e4f649e70882
Tomás Puchana <tpuchan33373@universidadean.edu.co>
2022-09-05 12:23:29 -0500

    ejercicio_tres

---
+++ Diccionarios/ejercicio_tres.py
@@ -0,0 +1,20 @@
+usuarios = { 
+ "iperurena": { 
+ "nombre": "Iñaki", 
+ "apellido": "Perurena", 
+ "password": "123123" 
+ }, 
+ "fmuguruza": { 
+ "nombre": "Fermín", 
+ "apellido": "Muguruza", 
+ "password": "654321" 
+ }, 
+ "aolaizola": { 
+ "nombre": "Aimar", 
+ "apellido": "Olaizola", 
+ "password": "123456"  } 
+ } 
+
+Usuario = print(input("Escriba su usuario: "))
+for clave in usuarios:
+    if Usuario 
